92a06f79b71d354a10f3f8cdc92f4a0cf98dbc76
Inferno <lucas.buckley@outlook.com>
2023-01-15 11:10:38 -0500

    Code for my test bot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thing.java
@@ -1,28 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
 
 @TeleOp
+
 public class thing extends LinearOpMode {
-    private DcMotor backRight;
-    private DcMotor backLeft;
-    private DcMotor frontRight;
-    private DcMotor frontLeft;
-    private DcMotor flywheel;
+    private ColorRangeSensor Distance;
+    private Servo Servo;
+    private DigitalChannel Touch;
+    private DcMotor Motor;
+
+    @Override
+
     public void runOpMode() {
-        backRight = hardwareMap.get(DcMotor.class,"back_right");
-        backLeft = hardwareMap.get(DcMotor.class,"back_left");
-        frontRight = hardwareMap.get(DcMotor.class,"front_right");
-        frontLeft = hardwareMap.get(DcMotor.class,"front_left");
-        flywheel = hardwareMap.get(DcMotor.class,"flywheel");
+        Distance = hardwareMap.get(ColorRangeSensor.class, "DISTANCE");
+        Servo = hardwareMap.get(Servo.class, "SERVO");
+        Touch = hardwareMap.get(DigitalChannel.class, "TOUCH");
+        Motor = hardwareMap.get(DcMotor.class, "johnSmithMotor");
+
+        boolean Toggle;
+        boolean PreviousValue;
+
+        Toggle = false;
+        PreviousValue = false;
+
+        Touch.setMode(DigitalChannel.Mode.INPUT);
+
         waitForStart();
-        powerFlywheel(1,10000);
-    }
-    public void powerFlywheel(double power,long duration) {
-        flywheel.setPower(power);
-        sleep(duration);
-        flywheel.setPower(0);
+
+        while (opModeIsActive()) {
+            if (!PreviousValue && Touch.getState()) {
+                Toggle = !Toggle;
+            }
+            PreviousValue = Touch.getState();
+            if (Toggle) {
+                Motor.setPower(0);
+                Servo.setPosition(1);
+            } else {
+                Motor.setPower(1.1 - (Distance.getDistance(DistanceUnit.CM)/10));
+                Servo.setPosition(Distance.getDistance(DistanceUnit.CM)/10);
+            }
+            telemetry.addData("Distance (cm)", Distance.getDistance(DistanceUnit.CM));
+            telemetry.update();
+        }
     }
 }
